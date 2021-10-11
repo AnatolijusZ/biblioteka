1a0412aa07b4ddbeae83a99bd1a6c812ba9ca444
AnatolijusZ <84323444+AnatolijusZ@users.noreply.github.com>
2021-10-11 23:45:09 +0300

    server plus main code

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import Books from "./Books";
+import NewBook from "./NewBook";
+import axios from "axios";
+import Top from "./Top";
+import Modal from "./Modal";
+
+function App() {
+  const [books, setBooks] = useState([]);
+  const [lastUpdate, setLastUpdate] = useState(Date.now());
+  const [modalId, setModalId] = useState(0);
+
+  useEffect(() => {
+    axios.get("http://localhost:3003/books")
+    .then((response) => {
+      setBooks(response.data);
+    });
+  }, [lastUpdate]);
+
+  const addBook = (book) => {
+    axios.post("http://localhost:3003/books", book)
+    .then(() => {
+      setLastUpdate(Date.now());
+    });
+  };
+
+  const deleteBook = (id) => {
+    axios.delete("http://localhost:3003/books/" + id)
+    .then(() => {
+      setLastUpdate(Date.now());
+    });
+  };
+
+  const getBook = id => {
+    if (id === 0) {
+    return [];
+    }
+    for(let i = 0; i < books.length; i++) {
+      if (books[i].id === id) {
+        return {...books[i]};
+      }
+    }
+  }
+
+  const showModal = id => {
+    setModalId(id)
+  }
+
+  return (
+    <>
+      <Top></Top>
+      <NewBook addBook={addBook} ></NewBook>
+      <Books books={books} deleteBook={deleteBook} showModal={showModal}></Books>
+      <Modal id={modalId} book={getBook(modalId)}></Modal>
+    </>
+  );
+}
+ 
+export default App;
